b8e0145b3a04b0d4fc512d84ac3ff318147fb03b
Sava <sava.paroski021@gmail.com>
2020-08-06 17:35:15 +0200

    Added component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import IdeaForm from './components/IdeaForm';
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div className="App container">
+      <IdeaForm />
     </div>
   );
 }
