98fda11399eb5bf3b752832badf4fe57386703c7
Sava <sava.paroski021@gmail.com>
2020-08-07 12:32:04 +0200

    Added firebase.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBAUI5wPOTG3H78jpoAv8nIoFKffDCM0bk",
+  authDomain: "idea-app-b57a9.firebaseapp.com",
+  databaseURL: "https://idea-app-b57a9.firebaseio.com",
+  projectId: "idea-app-b57a9",
+  storageBucket: "idea-app-b57a9.appspot.com",
+  messagingSenderId: "10799832846",
+  appId: "1:10799832846:web:a463032145f65d559f815f",
+  measurementId: "G-ZLD4PJY3GR"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
