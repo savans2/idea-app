0f3fdf6937b2932fe68a2c650eb0a135c87ad2a6
Sava <sava.paroski021@gmail.com>
2020-08-07 12:28:23 +0200

    Added components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
+
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App container d-flex flex-wrap">
       <IdeaForm />
+      <IdeaList />
     </div>
   );
 }
