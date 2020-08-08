9ce91d0300c7d11f3310cc93be7b73dc7e3dfc75
Sava <sava.paroski021@gmail.com>
2020-08-08 18:20:57 +0200

    Moved components to IdeaList component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
-
 
 function App() {
   return (
-    <div className="App container d-flex flex-wrap">
-      <IdeaForm />
+    <div className="App container">
       <IdeaList />
     </div>
   );
