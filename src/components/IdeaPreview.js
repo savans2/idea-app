262e3f8c3b5d0566af98d0d9af76b179b3372415
Sava <sava.paroski021@gmail.com>
2020-08-09 15:35:48 +0200

    Populated view with data.

--- src/components/IdeaPreview.js
+++ src/components/IdeaPreview.js
@@ -1,38 +1,33 @@
 import React from 'react'
 
-export default function IdeaPreview() {
+export default function IdeaPreview(props) {
+
+  let date = new Date(props.idea.date)
+  date = `${date.getDay()}.${date.getDate()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+  console.log(props.idea.shortName ? 'a' : 'b');
+
   return (
     <div className="bg-light my-3 col-12">
       <div className="p-3">
         <div className="blockquote">
           <div className="d-flex flex-wrap justify-content-between">
-            <p className="mb-0">1 Idea name that is a bit longer bla bla hey ho ha hi</p>
-            <p className="blockquote-footer mb-1 pl-2"><em>02.10.2020</em></p>
+            <p className="mb-0">{props.idea.shortName ? props.idea.shortName : 'No idea is selected ☹'}</p>
+            <p className="blockquote-footer mb-1 pl-2"><em>{props.idea.date ? date : '00.00.0000 00:00'}</em></p>
           </div>
-          <p className="blockquote-footer mb-1 pl-2">Category: <em>Work</em></p>
-          <p className="blockquote-footer mb-1 pl-2">Rating: <em>7/10</em></p>
+          <p className="blockquote-footer mb-1 pl-2">Category: <em>{props.idea.category ? props.idea.category : 'none'}</em></p>
+          <p className="blockquote-footer mb-1 pl-2">Rating: <em>{props.idea.rating ? props.idea.rating : 0}/10</em></p>
         </div>
         <div className="d-flex flex-wrap my-5">
           <div className="col-md-6">
             <h6>Description:</h6>
             <p className="pl-2">
-              Lorem ipsum dolor sit amet conse
-              ctetur adipisicing elit. Perferendis excepturi, ipsa
-              molestiae ducimus nobis,
-              inventore quos, id accusantium veniam
-              saepe asperiores porro dignissimos dolore praese
-              ntium ex veritatis alias error amet.
+              {props.idea.description ? props.idea.description : 'none'}
             </p>
           </div>
           <div className="col-md-6">
             <h6>Expectations:</h6>
             <p className="pl-2">
-              Lorem ipsum dolor sit amet conse
-              ctetur adipisicing elit. Perferendis excepturi, ipsa
-              molestiae ducimus nobis,
-              inventore quos, id accusantium veniam
-              saepe asperiores porro dignissimos dolore praese
-              ntium ex veritatis alias error amet.
+              {props.idea.expectations ? props.idea.expectations : 'none'}
             </p>
           </div>
         </div>
